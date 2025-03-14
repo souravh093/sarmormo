689e623c77b6055d0dbc1716e4dac47dceeb88d0
Sourave Halder <souravh093@gmail.com>
2025-03-14 23:22:17 +0600

    Hero section styling done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import { Button } from "@/components/ui/button";
+import BgGradient from "@/components/common/bg-gradient";
+import HeroSection from "@/components/home/hero-section";
 
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-      <Button className="bg-primary hover:bg-red-500">Click me</Button>
-    </>
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
+      {/* <DemoSection /> */}
+      {/* <HowItWorksSection /> */}
+      {/* <PricingSection /> */}
+      {/* <CTASection /> */}
+    </div>
   );
 }
---
+++ src/components/common/bg-gradient.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BgGradient = () => {
+  return (
+    <div>BgGradient</div>
+  )
+}
+
+export default BgGradient
