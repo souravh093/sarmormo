8d94b5fd173a10f67a495edd72882050979961d2
Sourave Halder <souravh093@gmail.com>
2025-03-14 21:50:13 +0600

    Project setup done with tailwindcss configuration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>Click me </Button>
-    </div>
+    <>
+      <h1>Home</h1>
+      <Button className="bg-primary hover:bg-red-500">Click me</Button>
+    </>
   );
 }
